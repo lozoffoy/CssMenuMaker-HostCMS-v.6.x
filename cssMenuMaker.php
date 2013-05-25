2351b9097dd0af90606293d76353d85e8db52356
Dmitry Lozovoy <lozoffoy@gmail.com>
2013-05-25 14:40:46 +0400

    decore

--- cssMenuMaker.php
+++ cssMenuMaker.php
@@ -1,18 +1,18 @@
 <?php
-	/* Базовые настройки показа меню */
-	$iMenu = 1; // идентификатор меню
-	$sXsl = 'CssMenuMaker'; // название XSL-шаблона
-	$bShowInformationsystemGroups = TRUE; // показываем в меню группы инфосистемы (TRUE/FALSE)
-	$bShowShopGroups = TRUE; // показываем в меню группы магазина (TRUE/FALSE)
+/* Базовые настройки показа меню */
+$iMenu = 1;															// идентификатор меню
+$sXsl = 'CssMenuMaker'; 								// название XSL-шаблона
+$bShowInformationsystemGroups = TRUE; 	// показываем в меню группы инфосистемы (TRUE/FALSE)
+$bShowShopGroups = TRUE; 								// показываем в меню группы магазина (TRUE/FALSE)
 
-	/* Код показа */
-  $Structure_Controller_Show = new Structure_Controller_Show(
-    Core_Entity::factory('Site', CURRENT_SITE));
+/* Код показа */
+$Structure_Controller_Show = new Structure_Controller_Show(
+	Core_Entity::factory('Site', CURRENT_SITE));
 
-  $Structure_Controller_Show->xsl(Core_Entity::factory('Xsl')
-    ->getByName($sXsl))
-    ->showInformationsystemGroups($bShowInformationsystemGroups)
-    ->showShopGroups($bShowShopGroups)
-    ->menu($iMenu)
-    ->show();
+$Structure_Controller_Show->xsl(Core_Entity::factory('Xsl')
+	->getByName($sXsl))
+	->showInformationsystemGroups($bShowInformationsystemGroups)
+	->showShopGroups($bShowShopGroups)
+	->menu($iMenu)
+	->show();
 ?>
